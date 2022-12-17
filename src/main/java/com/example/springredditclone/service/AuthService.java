d36dbeecd94524a19684ead969e2b011f2d64c4c
sharique08 <shariquemdzafar@gmail.com>
2022-12-17 20:09:26 +0530

    Adding mail templates and services

--- src/main/java/com/example/springredditclone/service/AuthService.java
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -2,22 +2,29 @@ package com.example.springredditclone.service;
 
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.model.User;
+import com.example.springredditclone.model.VerificationToken;
 import com.example.springredditclone.repository.UserRepository;
+import com.example.springredditclone.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Autowired
-    UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
+
+    @Transactional
     public void signup(RegisterRequest registerRequest){
          User user = new User();
          user.setUsername(registerRequest.username);
@@ -27,5 +34,17 @@ public class AuthService {
          user.setEnabled(false);
 
          userRepository.save(user);
+
+         String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken  = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
